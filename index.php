4aa0d5c303f724ae4440482b3669ebfabfed5ba5
ssingh <ssingh@iccsafe.org>
2015-08-03 11:16:32 +0530

    added for each loop for 100 tables

--- index.php
+++ index.php
@@ -8,4 +8,15 @@ echo "i added one more functionality";
 //this comment is added by the repositorty admin
 
 //hello i am making this chnage in development branch
+
+
+
+?>
+
+
+<?php
+for($i=0;$i<100;$i++){
+echo $i*1;
+
+}
 ?>
